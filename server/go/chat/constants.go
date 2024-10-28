fa94536454d1c1e2e630d9a3806c12ffe410d48b
Gabriele Santomaggio <G.santomaggio@gmail.com>
2024-10-28 17:59:37 +0100

    Documentation
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- server/go/chat/constants.go
+++ server/go/chat/constants.go
@@ -22,8 +22,8 @@ const (
 
 // / response codes
 const (
-	ResponseCodeOk uint16 = 0x0001
+	ResponseCodeOk uint16 = 0x01
 	//ResponseCodeError                   uint16 = 0x0002
-	ResponseCodeErrorUserNotFound      uint16 = 0x0003
-	ResponseCodeErrorUserAlreadyLogged uint16 = 0x0004
+	ResponseCodeErrorUserNotFound      uint16 = 0x03
+	ResponseCodeErrorUserAlreadyLogged uint16 = 0x04
 )
